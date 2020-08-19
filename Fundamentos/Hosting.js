d0b2ff4dc0f67f927822422b4590dd885fad9971
developer-luke <lucasfpf19@gmail.com>
2020-08-19 16:59:28 -0300

    Hosting with var

---
+++ Fundamentos/Hosting.js
@@ -0,0 +1,6 @@
+console.log('a =', a) // undefined
+var a = 2;
+console.log('a =', a) // a = 2
+
+// Variavel com 'var' é declarada de maneira global
+// Variavel dentro de função não é acessivel de fora
