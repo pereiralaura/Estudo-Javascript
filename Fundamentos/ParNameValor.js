075d5b9860c99da1112a8832cf440603f92368b1
developer-luke <lucasfpf19@gmail.com>
2020-08-19 17:08:08 -0300

    ParNomeValor e o contexto léxico

---
+++ Fundamentos/ParNameValor.js
@@ -0,0 +1,7 @@
+// par nome/valor
+
+const saudacao = 'opa' // contexto léxico 1 - local fisico onde foi variavel
+
+function exec(){
+    const saudacao = 'Falaaa' // contexto léxico 2 - local diferente aonde se localiza comparada ao primeiro
+}
