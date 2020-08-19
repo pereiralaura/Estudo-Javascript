75d85e9fd2d3f3199500f866c069dc7f8b607a43
developer-luke <lucasfpf19@gmail.com>
2020-08-19 10:40:15 -0300

    Math added

---
+++ Fundamentos/Math.js
@@ -0,0 +1,7 @@
+// Math = função da própria linguagem
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2) // Math.PI = número de PI -- Math.pow = potencia
+
+console.log(typeof Math) // Math é um objeto
+console.log(area) // pode usar um .toFixed()
