bb092c4b2fd7e59402af6d74ff50245714af62ea
developer-luke <lucasfpf19@gmail.com>
2020-08-19 17:05:01 -0300

    Objeto VS Funções

---
+++ Fundamentos/ObjectVSFunction.js
@@ -0,0 +1,25 @@
+console.log(typeof Object) // Função
+console.log(typeof new Object) // Objeto
+
+const cliente = function(){
+
+}
+
+console.log(typeof cliente) // Função
+console.log(typeof new cliente) // Objeto
+
+class Produto {
+
+}
+
+console.log(typeof Produto) // Função
+console.log(typeof new Produto()) // Objeto
+
+
+
+
+/* 
+
+    cria objeto a partir de função
+
+*/
