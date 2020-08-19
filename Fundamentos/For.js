758e2675f114b53fb8521b336938c67eb53294c6
developer-luke <lucasfpf19@gmail.com>
2020-08-19 16:46:36 -0300

    Added For and Var/Let

---
+++ Fundamentos/For.js
@@ -0,0 +1,4 @@
+for (var i = 0; i < 10; i++){ //define variavel - condição - incremento
+    console.log(i)
+}
+console.log("i =", i) // por ter sido definida com a palavra VAR, então ele pode ser acessado externamente
---
+++ Fundamentos/Variables.js
@@ -0,0 +1,3 @@
+var // escopo local e global
+let // escopo de bloco, local e global
+const // constante, ou seja, não altera o valor
