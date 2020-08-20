b13de98f59554247334a86c14167404b66cd929d
developer-luke <lucasfpf19@gmail.com>
2020-08-20 16:25:49 -0300

    Bind with This

---
+++ Funções/bind.js
@@ -0,0 +1,15 @@
+const pessoa = {
+    saudacao: 'Bom dia!',
+    falar (){ // função dentro do objeto
+        console.log(this.saudacao) // this = está ligado ao objeto
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // undefined - conflito Funcional e Orientado a Objetos (this mudou o local)
+
+const falardePessoa = pessoa.falar.bind(pessoa) // .bind() fará o this continuar apontando para o lugar correto
+falardePessoa() // problema resolvido com o Bind
+
+const falar2 = pessoa.falar
