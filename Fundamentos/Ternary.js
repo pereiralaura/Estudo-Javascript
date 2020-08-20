c8a5cb1552ab6eb18d1aab9855520ca63376ffbf
developer-luke <lucasfpf19@gmail.com>
2020-08-20 11:01:18 -0300

    Ternário

---
+++ Fundamentos/Ternary.js
@@ -0,0 +1,15 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+/* 
+
+    1ª Parte do Operador:
+        (nota >= 7) - expressão
+    2ª Parte do Operador:
+        verdadeiro = 'Aprovado' 
+    3ª Parte do Operador:
+        falso = 'Reprovado'
+
+*/
+
+console.log(resultado(7.1)) // teste = Aprovado
+console.log(resultado(4.3)) // teste = Reprovado
