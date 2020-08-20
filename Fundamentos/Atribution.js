ee3876621e48dc1b6658d70dcc23c127c99fd778
developer-luke <lucasfpf19@gmail.com>
2020-08-20 09:18:41 -0300

    Atribuição

---
+++ Fundamentos/Atribution.js
@@ -0,0 +1,18 @@
+const a = 7 // atribuindo 7 a 'a'
+let b = 3 // atribuindo 3 a 'b'
+
+b += a // b= b + a
+console.log(b)
+
+b-=4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
+
